03cbafcb20a530a836bcb01b2df0fc5c1492d1b7
Aidan Timson <aidan@timmo.dev>
2024-10-31 11:30:51 +0000

    Comment

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,28 @@ const PORT = 8080;
 
 const server = fastify();
 
-const redis = new Cluster(
-  [
-    {
-      host: Resource.ExampleFastifyRedis.host,
-      port: Resource.ExampleFastifyRedis.port,
-    },
-  ],
-  {
-    dnsLookup: (address, callback) => callback(null, address),
-    redisOptions: {
-      tls: {},
-      username: Resource.ExampleFastifyRedis.username,
-      password: Resource.ExampleFastifyRedis.password,
-    },
-  }
-);
+// const redis = new Cluster(
+//   [
+//     {
+//       host: Resource.ExampleFastifyRedis.host,
+//       port: Resource.ExampleFastifyRedis.port,
+//     },
+//   ],
+//   {
+//     dnsLookup: (address, callback) => callback(null, address),
+//     redisOptions: {
+//       tls: {},
+//       username: Resource.ExampleFastifyRedis.username,
+//       password: Resource.ExampleFastifyRedis.password,
+//     },
+//   }
+// );
 
 server.get("/", async (_request, _reply) => {
-  const counter = await redis.incr("counter");
+  // const counter = await redis.incr("counter");
   return {
     message: "Hello",
-    counter,
+    // counter,
   };
 });
 
