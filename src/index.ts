2b9fc00d959cefb5e1e74c100ac3651fc774b253
Aidan Timson <aidan@timmo.dev>
2024-10-31 10:26:27 +0000

    Setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/", async (_request, _reply) => {
+  return { message: "Hello" };
+});
+
+server.get("/ping", async (_request, _reply) => {
+  return "pong\n";
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
